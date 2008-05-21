278e150b4479fca5449cc280d425cebf945456a6
Damien Moore <damienlmoore@gmail.com>
2008-05-21 15:19:54 +0000

    ShellExtensions: reimplementing the Shell Control Widget as an abstract class that plugin developers can derive from. This will allow more flexible I/O redirection to the ShellManager notepages than can be accomodated simple process redirection (pipes). The new implementation will also include a registry for the developer defined Shell Controls (stolen from CBs plugin registry system). Implementation details still need to be added to ShellCtrlBase.cpp

---
+++ ShellExtensions/ShellCtrlBase.cpp
@@ -0,0 +1,186 @@
+#include <wx/notebook.h>
+#include <wx/textctrl.h>
+#include <wx/regex.h>
+#include "ShellCtrlBase.h"
+#include <globals.h>
+
+// The global instance of the shell registry
+ShellRegistry GlobalShellRegistry;
+
+////////////////////////////////////// ShellCtrlBase /////////////////////////////////////////////
+int ID_SHELLPOLLTIMER=wxNewId();
+int ID_PROC=wxNewId();
+int ID_SHELLMGR=wxNewId();
+
+
+//BEGIN_EVENT_TABLE(ShellCtrlBase, wxTextCtrl)
+//    EVT_CHAR(ShellCtrlBase::OnUserInput)
+//    EVT_END_PROCESS(ID_PROC, ShellCtrlBase::OnEndProcess)
+//    EVT_LEFT_DCLICK(ShellCtrlBase::OnDClick)
+//END_EVENT_TABLE()
+
+//IMPLEMENT_DYNAMIC_CLASS(ShellCtrlBase, wxFrame)
+//
+//
+//ShellCtrlBase::ShellCtrlBase(wxWindow* parent,wxWindowID id, ShellManager *shellmgr,
+//    const wxString& name,const wxString& value,const wxPoint& pos,const wxSize& size,long style)
+//    : wxTextCtrl(parent, id, value, pos, size, style)
+//{
+//    m_shellmgr=shellmgr;
+//    m_name=name;
+//    m_dead=true;
+//    m_proc=NULL;
+//    m_killlevel=0;
+//}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////// ShellManager /////////////////////////////////////////////
+
+BEGIN_EVENT_TABLE(ShellManager, wxPanel)
+    EVT_CHAR(ShellManager::OnUserInput)
+    EVT_TIMER(ID_SHELLPOLLTIMER, ShellManager::OnPollandSyncOutput)
+    EVT_FLATNOTEBOOK_PAGE_CLOSING(ID_SHELLMGR, ShellManager::OnPageClosing)
+END_EVENT_TABLE()
+
+
+void ShellManager::OnPageClosing(wxFlatNotebookEvent& event)
+{
+    ShellCtrlBase* sh = GetPage(event.GetSelection());
+    //    LOGSTREAM << wxString::Format(_T("OnPageClosing(): ed=%p, title=%s\n"), eb, eb ? eb->GetTitle().c_str() : _T(""));
+    if (!QueryClose(sh))
+        event.Veto();
+//    event.Skip(); // allow others to process it too
+}
+
+
+bool ShellManager::QueryClose(ShellCtrlBase* sh)
+{
+    if(!sh)
+        return true;
+    if(!sh->IsDead())
+    {
+        wxString msg(_("Process \"")+sh->GetName()+_("\" is still running...\nDo you want to kill it?"));
+        switch (cbMessageBox(msg, _("Kill process?"), wxICON_QUESTION | wxYES_NO))
+        {
+        case wxID_YES:
+            sh->KillProcess();
+            return false;
+        case wxID_NO:
+            return false;
+        }
+    }
+    return true;
+}
+
+
+long ShellManager::LaunchProcess(const wxString &processcmd, const wxString &name, const wxArrayString &options)
+{
+    int id=wxNewId();
+    // TODO: Need to figure out what type of object is being requested
+    ShellCtrlBase *shell=new ShellCtrlBase(m_nb,id,name,this);
+    long procid=shell->LaunchProcess(processcmd,ParseLinks,LinkClicks,LinkRegex);
+    if(procid>0)
+    {
+        if(!m_synctimer.IsRunning())
+            m_synctimer.Start(100);
+    }
+    else
+    {
+        cbMessageBox(_T("process launch failed."));
+        delete shell;
+        return -1;
+    }
+    m_nb->AddPage(shell,name);
+    m_nb->SetSelection(m_nb->GetPageCount()-1);
+    return procid;
+}
+
+ShellCtrlBase *ShellManager::GetPage(size_t i)
+{
+    return (ShellCtrlBase*)m_nb->GetPage(i);
+}
+
+ShellCtrlBase *ShellManager::GetPage(const wxString &name)
+{
+    for(int i=0;i<m_nb->GetPageCount();i++)
+    {
+        ShellCtrlBase *sh=GetPage(i);
+        if(name==sh->GetName())
+            return sh;
+    }
+    return NULL;
+}
+
+
+// Forecefully kill the process
+void ShellManager::KillProcess(int id)
+{
+}
+
+void ShellManager::KillWindow(int id)
+{
+}
+
+size_t ShellManager::GetTermNum(ShellCtrlBase *term)
+{
+    for(int i=0;i<m_nb->GetPageCount();i++)
+    {
+        ShellCtrlBase *shell=GetPage(i);
+        if(shell==term)
+            return i;
+    }
+    return m_nb->GetPageCount();
+}
+
+int ShellManager::NumAlive()
+{
+    int count=0;
+    for(int i=0;i<m_nb->GetPageCount();i++)
+        count+=!GetPage(i)->IsDead();
+    return count;
+}
+
+
+void ShellManager::OnShellTerminate(ShellCtrlBase *term)
+{
+    size_t i=GetTermNum(term);
+    m_nb->SetPageText(i,_T("[DONE]")+m_nb->GetPageText(i));
+    if(NumAlive()==0)
+        m_synctimer.Stop();
+}
+
+
+void ShellManager::OnPollandSyncOutput(wxTimerEvent& te)
+{
+    for(int i=0;i<m_nb->GetPageCount();i++)
+    {
+        GetPage(i)->ReadStream();
+    }
+}
+
+void ShellManager::OnUserInput(wxKeyEvent& ke)
+{
+    ShellCtrlBase *sh=(ShellCtrlBase*)m_nb->GetCurrentPage();
+    sh->OnUserInput(ke);
+}
+
+ShellManager::ShellManager(wxWindow* parent)
+    : wxPanel(parent, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL/* | wxCLIP_CHILDREN*/)
+{
+    m_synctimer.SetOwner(this, ID_SHELLPOLLTIMER);
+    wxBoxSizer* bs = new wxBoxSizer(wxVERTICAL);    m_nb = new wxFlatNotebook(this, ID_SHELLMGR, wxDefaultPosition, wxDefaultSize, wxFNB_X_ON_TAB|wxFNB_NO_X_BUTTON);    bs->Add(m_nb, 1, wxEXPAND | wxALL);
+    SetAutoLayout(TRUE);
+    SetSizer(bs);
+}
+
+ShellManager::~ShellManager()
+{
+    //dtor
+    //All of the subwindows owned by this panel will be destroyed automatically
+}
+
---
+++ ShellExtensions/ShellCtrlBase.h
@@ -0,0 +1,160 @@
+#ifndef SHELLCTRL_H
+#define SHELLCTRL_H
+
+#include <wx/wxprec.h>
+
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif
+
+#include <wx/process.h>
+#include <wx/wxFlatNotebook/wxFlatNotebook.h>
+#include <sdk.h>
+#include <vector>
+
+// CLASSES DEFINED IN THIS LIBRARY
+class ShellManager; //Manages the collection of Shell Control Widgets allowing user interaction with external processes within a tabbed notepage - usually the main app (or some plugin) will declare a global instance of this manager. See the full declaration below for more detail
+class ShellCtrlBase; //The manager manages a set of Shell Control widgets that redirect I/O from an external process - ShellCtrlBase is an abstract base class allowing the developer to create their own custom controls for handling I/O from their process. Note that "process" is not defined here, it could be a new thread, an external program, a server connection etc
+class ShellRegInfo; //Every custom shell control must provide basic info to a global registry
+class ShellRegistry; //The global registry stores the info for all known custom shell controls
+template<class T> class ShellCtrlRegistrant; //The developer makes their custom Shell Control classes available to the manager (and the main application) by creating an instance of this template class
+
+typedef ShellCtrlBase*(*fnCreate)(); //typedef defining function to create a custom shell control widget
+typedef void(*fnFree)(ShellCtrlBase*); //typedef defining function to free a custom shell control widget
+
+//Every type of shell control has the following registration info
+class ShellRegInfo
+{
+    wxString name; //unique name of the type
+    fnCreate create; //static function call needed to create instance on the heap
+    fnFree free; //static function call needed to free instance
+};
+
+// Before shells can be used they must be registered in a ShellRegistry, which
+// represents a collection of registered shell controls. One global instance is created
+// within this library
+class ShellRegistry
+{
+    bool Register(const wxString &name, fnCreate create, fnFree free); //register/deregister are called by the plugin registrant instance
+    bool Deregister(const wxString &name);
+    ShellCtrlBase *CreateControl(const wxString &type,wxWindow* parent, int id, const wxString &windowname, ShellManager *shellmgr=NULL); //TODO: returns null if type doesn't exist in registry
+    ShellCtrlBase *FreeControl(const wxString &type);
+    std::vector<ShellRegInfo> m_reginfo;
+};
+
+extern ShellRegistry GlobalShellRegistry; //defined in shellctrlbase.cpp, but accessible to all linking libraries
+
+// every library that creates a new shell control must create an instance of this class with the type of their control as the template paramater T. This will add the new class to the registry of shell controls and the needed functions to create new instances
+template<class T> class ShellCtrlRegistrant
+{
+    public:
+        /// @param name The name of the ShellCtrl.
+        ShellCtrlRegistrant(const wxString& name)
+        {
+            m_name=name;
+            GlobalShellRegistry.Register(name, &Create, &Free);
+        }
+        ~ShellCtrlRegistrant()
+        {
+            GlobalShellRegistry.Deregister(m_name);
+        }
+        static ShellCtrlBase* Create() //allocates new shell control object on heap
+        {
+            return new T;
+        }
+
+        static void Free(ShellCtrlBase* sh) // deletes object from heap
+        {
+            delete sh;
+        }
+        wxString m_name;
+
+//        static void SDKVersion(int* major, int* minor, int* release)
+//        {
+//            if (major) *major = PLUGIN_SDK_VERSION_MAJOR;
+//            if (minor) *minor = PLUGIN_SDK_VERSION_MINOR;
+//            if (release) *release = PLUGIN_SDK_VERSION_RELEASE;
+//        }
+};
+
+
+
+// This is the shell control base class
+// All controls derives from a basic wxWindow
+// The control must offer services to create and destroy underlying processes
+// terminal controls sit inside a tabbed panel managed by the Shell Manager
+class ShellCtrlBase : public wxWindow //TODO: make wxWindow a member, not a base??
+{
+    public:
+        //ShellCtrlBase() {m_dead=true; m_id=-1}
+        ShellCtrlBase(wxWindow* parent, int id, const wxString &name, ShellManager *shellmgr=NULL)
+                : wxWindow(parent, id)
+            {m_parent=parent;
+             m_id=id; m_dead=true;
+             m_shellmgr=shellmgr; }
+        virtual ~ShellCtrlBase();
+
+        // Every shell control widget must override the following
+        virtual long LaunchProcess(const wxString &processcmd, const wxArrayString &options)=0;
+        virtual void KillFrame()=0; // manager may destroy the window, but will call this before doing so
+        virtual void KillProcess()=0;
+        virtual void SyncOutput(int maxchars=1000)=0; //use this to respond to ShellManager request to gather output from the running process for display in the frame
+
+        wxString GetName() {return m_name;}
+        void SetName(const wxString &name) {m_name=name;}
+        bool IsDead() {return m_dead;}
+    private:
+        wxString m_name;
+    protected:
+        ShellManager *m_shellmgr;
+        bool m_dead;
+        int m_id;
+    DECLARE_DYNAMIC_CLASS(wxFrame)
+//    DECLARE_EVENT_TABLE()
+};
+
+// abstract base class to manage collections of shell terms
+//class ShellManager
+//{
+//    friend class ShellCtrlBase;
+//    public:
+//        virtual ~ShellManager();
+//    private:
+//        virtual void OnShellTerminate(ShellCtrlBase *term); //notifies manager of shell termination
+//};
+
+
+class ShellManager : public wxPanel
+{
+    friend class ShellCtrlBase;
+    public:
+        ShellManager(wxWindow* parent);
+        ~ShellManager(); //virtual??
+        long LaunchProcess(const wxString &processcmd, const wxString &name, const wxArrayString &options);
+        void KillProcess(int id);
+        void KillWindow(int id);
+        ShellCtrlBase *GetPage(size_t i);
+        ShellCtrlBase *GetPage(const wxString &name);
+        int NumAlive();
+    private:
+        //Responders to standard wxWidgets Messages
+        void OnUserInput(wxKeyEvent& ke);
+        void OnPollandSyncOutput(wxTimerEvent& te);
+        void OnPageClosing(wxFlatNotebookEvent& event);
+        bool QueryClose(ShellCtrlBase* sh);
+        //Responders to friend class ShellCtrlBase
+        void OnShellTerminate(ShellCtrlBase *term);
+        size_t GetTermNum(ShellCtrlBase *term);
+    protected:
+        wxTimer m_synctimer;
+//#ifdef CBIL_TEARAWAY
+//        TearawayNotebook *m_nb;
+//#else
+        wxFlatNotebook *m_nb;
+//#endif
+    DECLARE_EVENT_TABLE()
+};
+
+
+
+#endif // SHELLCTRL_H

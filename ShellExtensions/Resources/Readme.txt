00a211648398c9a06a468fc37c303d7fbf0dce59
Damien Moore <damienlmoore@gmail.com>
2011-02-06 16:20:02 +0000

    Removed defunct ShellExtensions from trunk

--- ShellExtensions/Resources/Readme.txt
+++
@@ -1,1 +0,0 @@
-Any resources in this folder are not required for compilation. (They are used to produce the source code for Dialog boxes)
--- ShellExtensions/pyeval.py
+++
@@ -1,9 +0,0 @@
-import os
-print "working in",os.getcwd()
-print "welcome"
-print ">",
-s=raw_input()
-while s!='exit':
-    print "you entered:",s
-    print ">",
-    s=raw_input()
